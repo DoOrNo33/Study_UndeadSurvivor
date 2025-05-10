2a95bfa84ada4d703cd6b548cbc456c70e17588e
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2025-05-10 11:18:50 +0900

    [GM] 03_2D셀애니메이션
    같은 로직을 가진 애니메이션을 이미지만 돌려서 쓸 때는 애니메이터 오버라이드 컨트롤러를 이용하자

--- Assets/01_Scripts/Player.cs
+++ Assets/01_Scripts/Player.cs
@@ -6,11 +6,16 @@ public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
     public float speed;
+
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -22,5 +27,15 @@ public class Player : MonoBehaviour
     private void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+    }
+
+    private void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;     // 좌측 이동할 때 inputVec.x < 0 이므로 flipX = true, 우측 이동할 때 inputVec.x > 0 이므로 flipX = false
+        }
     }
 }
