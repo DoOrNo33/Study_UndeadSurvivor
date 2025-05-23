32cdd28be4d49d1967addf58425a60a041047284
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2025-05-23 13:22:13 +0900

    [GM] 08_자동원거리공격구현

--- Assets/01_Scripts/Weapon.cs
+++ Assets/01_Scripts/Weapon.cs
@@ -9,6 +9,14 @@ public class Weapon : MonoBehaviour
     public int count;
     public float speed;
 
+    private float timer;
+    private Player player;
+
+    private void Awake()
+    {
+        player = GetComponentInParent<Player>();
+    }
+
     private void Start()
     {
         Init();
@@ -22,14 +30,20 @@ public class Weapon : MonoBehaviour
                 transform.Rotate(Vector3.back * speed * Time.deltaTime);
                 break;
             default:
+                timer += Time.deltaTime;
 
+                if (timer > speed)
+                {
+                    timer = 0;
+                    Fire();
+                }
                 break;
         }
 
         // .. Test Code..
         if (Input.GetButtonDown("Jump"))
         {
-            LevelUp(20, 5);
+            LevelUp(10, 1);
         }
     }
 
@@ -52,7 +66,7 @@ public class Weapon : MonoBehaviour
                 Batch();
                 break;
             default:
-
+                speed = 0.3f;
                 break;
         }
     }
@@ -79,7 +93,22 @@ public class Weapon : MonoBehaviour
             Vector3 rotVec = Vector3.forward * 360 * i / count;
             bullet.Rotate(rotVec);
             bullet.Translate(bullet.up * 1.5f, Space.World);
-            bullet.GetComponent<Bullet>().Init(damage, -1); // -1 is Infinity Per.
+            bullet.GetComponent<Bullet>().Init(damage, -1, Vector3.zero); // -1 is Infinity Per.
         }
+    }
+
+    private void Fire()
+    {
+        if (!player.scanner.nearestTarget)
+            return;
+
+        Vector3 targetPos = player.scanner.nearestTarget.position;
+        Vector3 dir = targetPos - transform.position;
+        dir = dir.normalized;
+
+        Transform bullet = GameManager.instance.pool.Get(prefabId).transform;
+        bullet.position = transform.position;
+        bullet.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+        bullet.GetComponent<Bullet>().Init(damage, count, dir);
     }
 }
